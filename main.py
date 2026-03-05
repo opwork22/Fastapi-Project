f0b006e701df2a24b008f437ae5c9a74e9ebb649
opwork22 <opwork22@gmail.com>
2026-03-05 08:07:55 +0530

    update

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI(title="User Notes API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https:/google.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
